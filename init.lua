a696dd27bb53f4e568f0b9e758f4d80e46582691
Sebastian Lara Menares <slara@mtsolutions.io>
2023-04-02 16:28:04 -0400

    configure lua_ls

--- init.lua
+++ init.lua
@@ -57,7 +57,6 @@ require('lazy').setup({
 
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
-
   { -- Adds git releated signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
@@ -372,6 +371,12 @@ local servers = {
   -- pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
+  lua_ls = {
+    Lua = {
+      workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
+    },
+  },
 }
 
 -- Setup neovim lua configuration
@@ -409,6 +414,8 @@ require("flutter-tools").setup{} -- use defaults
 -- nvim-cmp setup
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
+
+luasnip.config.setup {}
 
 cmp.setup {
   snippet = {
