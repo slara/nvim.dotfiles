63ef5885f832eab8a185832e86fccd88dd35d7f9
Sebastian Lara Menares <slara@mtsolutions.io>
2024-02-27 21:28:29 -0300

    add nvim-web-devicons

--- init.lua
+++ init.lua
@@ -132,6 +132,7 @@ require('lazy').setup({
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
       -- Fuzzy Finder Algorithm which requires local dependencies to be built.
       -- Only load if `make` is available. Make sure you have the system
       -- requirements installed.
