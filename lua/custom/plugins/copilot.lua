a1e86cd2b75a201c77c4e0b84c855e98abe17f68
Sebastian Lara Menares <slara@mtsolutions.io>
2025-07-06 20:35:09 -0400

    fix completion and telescope grep

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -5,14 +5,14 @@ return {
     event = "InsertEnter",
     config = function()
       vim.g.copilot_no_tab_map = true
-      vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+      vim.api.nvim_set_keymap("i", "<TAB>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
       require("copilot").setup({
         suggestion = {
           enabled = true,
           auto_trigger = true,
           debounce = 75,
           keymap = {
-            accept = "<C-J>",
+            accept = "<TAB>",
             accept_word = false,
             accept_line = false,
             next = "<M-]>",
