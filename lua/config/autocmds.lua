40e20aeead0bf43e8de6393db1c76cec2dc4abc4
Sebastian Lara Menares <slara@mtsolutions.io>
2026-04-02 21:59:21 -0300

    improve claude snippets

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,14 @@
+-- Auto-change local window directory to current file's directory
+local lcd_group = vim.api.nvim_create_augroup('AutoLcd', { clear = true })
+vim.api.nvim_create_autocmd('BufEnter', {
+  group = lcd_group,
+  callback = function()
+    if vim.bo.buftype == '' then
+      vim.cmd('lcd %:p:h')
+    end
+  end,
+})
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
